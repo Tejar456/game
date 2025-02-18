2b25fbd5de626fb1c1430891eadfad95521273a5
tejar <tejarm04@gmail.com>
2025-02-18 14:08:32 +0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <img
+      src="/assets//img/bg.png"
+      alt="bg"
+      class="h-screen w-full fixed top-0 right-0 -z-10 object-cover"
+    />
+    <img src="/assets/img/button.png" alt="btn" class="w-1/2" />
+  </div>
+</template>
